ceafbd9c765017f673947b7c3e77f04f4e27b2df
Rodrigo Barraza <hello@rod.dev>
2026-07-14 01:08:20 -0700

    feat: inject user identity into session tracker and forward additional client headers to proxy route

--- src/components/SessionTrackerComponent.tsx
+++ src/components/SessionTrackerComponent.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { SessionTrackerComponent as LibrarySessionTracker } from "@rodrigo-barraza/components-library";
+import { useSession } from "next-auth/react";
 import { PROJECT_NAME } from "@/config";
 
 export default function SessionTrackerComponent() {
-  return <LibrarySessionTracker projectId={PROJECT_NAME} />;
+  const { data: userSession } = useSession();
+  const userId = userSession?.user?.email || userSession?.user?.name || null;
+
+  return <LibrarySessionTracker projectId={PROJECT_NAME} userId={userId} />;
 }
